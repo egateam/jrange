9159e05109ad10ee61f8350432a07ac2595b07d4
wang-q <wang-q@outlook.com>
2016-06-16 23:02:30 +0800

    Example codes

---
+++ src/main/java/com/github/egateam/commands/Merge.java
@@ -0,0 +1,59 @@
+/**
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY DISCLAIMED.
+ */
+
+package com.github.egateam.commands;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.github.egateam.util.FileConverterIn;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings({"CanBeFinal"})
+@Parameters(commandDescription = "Merge runlist yaml files")
+public class Merge {
+
+    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+    @Parameter(description = "<infiles>", converter = FileConverterIn.class, required = true)
+    private List<File> files;
+
+    @Parameter(names = {"--outfile", "-o"}, description = "Output filename. [stdout] for screen.")
+    private String outfile;
+
+    private void validateArgs() {
+//        if ( files.size() < 1 ) {
+//            throw new ParameterException("This command need one or more input files.");
+//        }
+
+        if ( outfile == null ) {
+            outfile = files.get(0) + ".merge.yml";
+        }
+    }
+
+    public void execute() throws Exception {
+        validateArgs();
+
+        //----------------------------
+        // Loading
+        //----------------------------
+//        Map<String, Map<String, ?>> master = new HashMap<>();
+//        for ( File inFile : files ) {
+//            Map<String, ?> map = StaticUtils.readRl(inFile);
+//
+//            String basename = FilenameUtils.getBaseName(inFile.toString());
+//            master.put(basename, map);
+//        }
+//
+//        //----------------------------
+//        // Output
+//        //----------------------------
+//        StaticUtils.writeRl(outfile, master);
+    }
+}
