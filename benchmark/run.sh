94589bba50cb75ad14e1ffb53df6c7133cf7f97c
wang-q <wang-q@outlook.com>
2016-06-18 03:09:53 +0800

    Materials for benchmark

---
+++ benchmark/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cpanm App::RL App::Fasops
+
+COMMAND_TIME="command time -v"
+if [[ `uname` == 'Darwin' ]];
+then
+    COMMAND_TIME="command time -l"
+fi
+
+echo "==> jrange"
+${COMMAND_TIME} java -jar ../target/jrange-*-jar-with-dependencies.jar \
+    merge \
+    -o stdout -c 0.95 \
+    links.lastz.tsv \
+    links.blast.tsv \
+    > /dev/null
+
+echo "==> merge_node.pl"
+${COMMAND_TIME} perl ~/Scripts/egas/merge_node.pl \
+    -v -c 0.95 \
+    -f links.lastz.tsv \
+    -f links.blast.tsv \
+    > /dev/null
