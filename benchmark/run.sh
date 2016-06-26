018bdce13e3fd27bf80812091672b58d67a9382f
wang-q <wang-q@outlook.com>
2016-06-26 09:35:22 +0800

    Add another benchmark

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -8,7 +8,7 @@ then
     COMMAND_TIME="command time -l"
 fi
 
-echo "==> jrange"
+echo "==> jrange lastz blast"
 ${COMMAND_TIME} java -jar ../target/jrange-*-jar-with-dependencies.jar \
     merge \
     -o stdout -c 0.95 \
@@ -16,10 +16,24 @@ ${COMMAND_TIME} java -jar ../target/jrange-*-jar-with-dependencies.jar \
     links.blast.tsv \
     > /dev/null
 
-echo "==> App::Rangeops"
+echo "==> jrange sort.clean"
+${COMMAND_TIME} java -jar ../target/jrange-*-jar-with-dependencies.jar \
+    merge \
+    -o stdout -c 0.95 \
+    sort.clean.tsv \
+    > /dev/null
+
+echo "==> App::Rangeops lastz blast"
 ${COMMAND_TIME} rangeops \
     merge \
     -o stdout -c 0.95 -p 8 \
     links.lastz.tsv \
     links.blast.tsv \
+    > /dev/null
+
+echo "==> App::Rangeops sort.clean"
+${COMMAND_TIME} rangeops \
+    merge \
+    -o stdout -c 0.95 -p 8 \
+    sort.clean.tsv \
     > /dev/null
