ed8975a7a8cbf63cbf0e17bf62a4bc811123e21d
wang-q <wang-q@outlook.com>
2017-06-19 07:20:01 +0800

    Test materials

---
+++ src/test/java/com/github/egateam/jrange/commands/CoveredTest.java
@@ -0,0 +1,58 @@
+package com.github.egateam.jrange.commands;
+
+import com.github.egateam.commons.Utils;
+import com.github.egateam.jrange.Cli;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+public class CoveredTest {
+    // Store the original standard out before changing it.
+    private final PrintStream           originalStdout = System.out;
+    private final PrintStream           originalStderr = System.err;
+    private       ByteArrayOutputStream stdoutContent  = new ByteArrayOutputStream();
+    private       ByteArrayOutputStream stderrContent  = new ByteArrayOutputStream();
+
+    @BeforeMethod
+    public void beforeTest() {
+        // Redirect all System.out to stdoutContent.
+        System.setOut(new PrintStream(this.stdoutContent));
+        System.setErr(new PrintStream(this.stderrContent));
+    }
+
+    @Test
+    public void testArgumentsFailed() throws Exception {
+        String[] args = {"covered"};
+        Cli.main(args);
+
+        Assert.assertTrue(this.stderrContent.toString().contains("Main parameters are required"),
+            "Except parameters");
+    }
+
+    @Test(description = "Test command covered")
+    public void testExecute() throws Exception {
+        String   fileName1 = Utils.expendResource("1_4.pac.paf.ovlp.tsv");
+        String[] args      = {"covered", fileName1, "--outfile", "stdout"};
+        Cli.main(args);
+
+        Assert.assertEquals(this.stdoutContent.toString().split("\r\n|\r|\n").length, 8, "line count");
+        Assert.assertTrue(this.stdoutContent.toString().contains("pac4745_7148"), "original names");
+        Assert.assertFalse(this.stdoutContent.toString().contains("pac4745_7148:1"), "uncovered region");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        // Put back the standard out.
+        System.setOut(this.originalStdout);
+        System.setErr(this.originalStderr);
+
+        // Clear the stdoutContent.
+        this.stdoutContent = new ByteArrayOutputStream();
+        this.stderrContent = new ByteArrayOutputStream();
+    }
+}
